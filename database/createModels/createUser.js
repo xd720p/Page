9d5e21306bcaf6314ae720ac178d3d2f9ed40523
xd720p <xd720p@gmail.com>
2016-06-13 21:56:31 +0300

    Auto

--- database/createModels/createUser.js
+++ database/createModels/createUser.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 var sequelize = require('./connect');
-
+var crypto = require('crypto');
 
 var user = sequelize.define('user', {
     name: {
@@ -8,21 +8,30 @@ var user = sequelize.define('user', {
         field: 'name', // Will result in an attribute that is firstName when user facing but first_name in the database
         primaryKey: true
     },
-    password: {
-        type: Sequelize.STRING(90),
-        primaryKey: true
+    hash: {
+        type: Sequelize.STRING(90)
+
+    },
+    salt: {
+        type: Sequelize.STRING(90)
+
     }
 },{
     timestamps: false,
     freezeTableName: true, // Model tableName will be the same as the model name
     classMethods: {
-        checkPassword: function(username, password, callback){
+        /*checkPassword: function(username, password, callback){
             this.findOne({where:{name: username, password: password}}).then(function (user) {
                 if (user == null) callback(null, "USERNAME OR PASSWORD NOT MATCH");
                 else callback(user, null);
             });
-        }
+        }*/
+        
+        setPassword: function (password) {
+            this.salt = crypto.randomBytes(16).toString('hex');
 
+            this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+        }
     }
 });
 
